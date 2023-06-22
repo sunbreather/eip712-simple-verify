c129c889085d6a559375e91e3fb64ae42d163b45
sunbreather <hinokami_kagura0@protonmail.com>
2023-06-22 17:46:05 -0500

    wip

--- src/Verify.sol
+++ src/Verify.sol
@@ -1,54 +1,35 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.19;
 
-contract Verify {
-    bytes32 public immutable EIP712_DOMAIN_TYPE;
-    bytes32 public constant APP_NAME = keccak256(bytes("Verify"));
-    bytes32 public constant VERSION = keccak256(bytes("1"));
-    bytes32 public constant EIP712_DOMAIN_TYPEHASH =
-        keccak256(
-            "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
-        );
-    bytes32 public constant SIG_VOTE_TYPEHASH =
-        keccak256("SignedObject(uint256 Id,address sender)");
+import "@openzeppelin-contracts/contracts/utils/cryptography/EIP712.sol";
+
+contract Verify is EIP712 {
+    bytes32 constant SIGNED_OBJECT_TYPEHASH =
+        keccak256("SignedObject(uint256 value)");
 
     struct SignedObject {
-        uint256 Id;
-        address sender;
+        uint256 value;
     }
 
-    constructor() {
-        EIP712_DOMAIN_TYPE = keccak256(
-            abi.encode(
-                EIP712_DOMAIN_TYPEHASH,
-                APP_NAME,
-                VERSION,
-                block.chainid,
-                address(this)
-            )
-        );
-    }
+    constructor() EIP712("Verify", "1") {}
 
     function verify(
         SignedObject calldata signedObject,
+        address _signer,
         uint8 v,
         bytes32 r,
         bytes32 s
-    ) external {
-        bytes32 hash_ = keccak256(
-            abi.encodePacked(
-                "\x19\x01",
-                EIP712_DOMAIN_TYPE,
+    ) external view {
+        address signer = ECDSA.recover(
+            _hashTypedDataV4(
                 keccak256(
-                    abi.encode(
-                        SIG_VOTE_TYPEHASH,
-                        signedObject.Id,
-                        signedObject.sender
-                    )
+                    abi.encode(SIGNED_OBJECT_TYPEHASH, signedObject.value)
                 )
-            )
+            ),
+            v,
+            r,
+            s
         );
-        address _signer = ecrecover(hash_, v, r, s);
-        require(signedObject.sender == _signer, "Invalid signature");
+        require(signer == _signer, "Invalid signature");
     }
 }
--- test/Verify.t.sol
+++ test/Verify.t.sol
@@ -3,11 +3,43 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/Verify.sol";
+import "@openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
 
 contract VerifyTest is Test {
+    Verify verify;
 
+    bytes32 public constant EIP712_DOMAIN_TYPEHASH =
+        keccak256(
+            "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+        );
+    bytes32 constant SIGNED_OBJECT_TYPEHASH =
+        keccak256("SignedObject(uint256 value)");
+    bytes32 constant APP_NAME = keccak256(bytes("Verify"));
+    bytes32 constant VERSION = keccak256(bytes("1"));
 
     function setUp() public {
+        verify = new Verify();
+    }
 
+    function testVerify() public {
+        bytes32 EIP712_DOMAIN_TYPE = keccak256(
+            abi.encode(
+                EIP712_DOMAIN_TYPEHASH,
+                APP_NAME,
+                VERSION,
+                block.chainid,
+                address(verify)
+            )
+        );
+
+        bytes32 hash_ = ECDSA.toTypedDataHash(
+            EIP712_DOMAIN_TYPE,
+            keccak256(abi.encode(SIGNED_OBJECT_TYPEHASH, 1))
+        );
+
+        Verify.SignedObject memory so = Verify.SignedObject(1);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(1, hash_);
+        verify.verify(so, vm.addr(1), v, r, s);
     }
 }
